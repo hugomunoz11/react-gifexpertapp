b1dedc351df95d716f4d24207fccadd624ec8597
Hugo Muñoz <hugo.gmc17@gmail.com>
2022-04-07 08:15:41 -0700

    First commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import {getGif} from "../helpers/getGif.js"
+
+export const useFetchGifs = (category) => {
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect(() => {
+        getGif(category)
+        .then(imgs => {
+            setState({
+                data: imgs,
+                loading: false
+            });
+        })
+    },[category]);
+
+    return state;
+}
